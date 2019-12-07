c24ffe5a5a3a7b596c9023f2df75e9e61a8ed9b3
wsc <616758136@qq.com>
2019-12-07 15:11:33 +0800

    ff

---
+++ GitTEST/GitTEST/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTEST
+//
+//  Created by Apple on 2019/12/7.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ GitTEST/GitTEST/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTEST
+//
+//  Created by Apple on 2019/12/7.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
