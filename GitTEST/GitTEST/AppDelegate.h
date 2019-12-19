aea4e788c982165713163207023b27d8b39ba274
wsc <616758136@qq.com>
2019-12-19 14:55:34 +0800

    //测试4

--- GitTEST/GitTEST/AppDelegate.h
+++ GitTEST/GitTEST/AppDelegate.h
@@ -5,7 +5,7 @@
 //  Created by Apple on 2019/12/7.
 //  Copyright © 2019 Apple. All rights reserved.
 //
-
+//测试4
 #import <UIKit/UIKit.h>
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 //炒作1
