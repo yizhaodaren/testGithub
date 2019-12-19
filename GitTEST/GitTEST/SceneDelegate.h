88f9600d607cb01ce623e7c5953e3d493e82c8a3
wsc <616758136@qq.com>
2019-12-19 15:09:42 +0800

    //master3

--- GitTEST/GitTEST/SceneDelegate.h
+++ GitTEST/GitTEST/SceneDelegate.h
@@ -5,7 +5,7 @@
 //  Created by Apple on 2019/12/7.
 //  Copyright © 2019 Apple. All rights reserved.
 //
-
+//master3
 #import <UIKit/UIKit.h>
 //炒作1
 @interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
